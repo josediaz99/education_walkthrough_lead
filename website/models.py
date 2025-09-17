e5dd905e138526500a3bbbf397665635e7651e85
Matthew Lee <67172710+mylee2662@users.noreply.github.com>
2025-09-17 14:28:44 -0500

    UI + Models

--- website/models.py
+++ website/models.py
@@ -2,6 +2,12 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
+document_tag = db.Table(
+    'document_tag',
+    db.Column('document_id', db.Integer, db.ForeignKey('document.id'), primary_key=True),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True)
+)
+
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
@@ -14,5 +20,31 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
     notes = db.relationship('Note')
+    schools = db.relationship('School')
 
 
+class School(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150))
+    address = db.Column(db.String(150))
+    city = db.Column(db.String(150))
+    state = db.Column(db.String(150))
+    zip_code = db.Column(db.String(10))
+    phone_number = db.Column(db.String(20))
+    email = db.Column(db.String(150), unique=True)
+    website = db.Column(db.String(150))
+    documents = db.relationship('Document')
+    tags = db.relationship('Tag', secondary=document_tag, backref='schools')
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class Document(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150))
+    url = db.Column(db.String(150))
+    upload_date = db.Column(db.DateTime(timezone=True), default=func.now())
+    school_id = db.Column(db.Integer, db.ForeignKey('school.id'))
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+    description = db.Column(db.String(150))
--- website/views.py
+++ website/views.py
@@ -13,14 +13,35 @@ views = Blueprint('views', __name__)
 @login_required
 def home():
     if request.method == 'POST':
-        note = request.form.get('note')
-        if len(note) < 1:
-            flash("Note is too short!", category='error')
-        else:
-            new_note = Note(data=note, user_id=current_user.id)
-            db.session.add(new_note)
-            db.session.commit()
-            flash("Note Added!", category='success')
+        # note = request.form.get('note')
+        # if len(note) < 1:
+        #     flash("Note is too short!", category='error')
+        # else:
+        #     new_note = Note(data=note, user_id=current_user.id)
+        #     db.session.add(new_note)
+        #     db.session.commit()
+        #     flash("Note Added!", category='success')
+        name = request.form.get('name')
+        address = request.form.get('address')
+        city = request.form.get('city')
+        state = request.form.get('state')
+        zip_code = request.form.get('zip_code')
+        phone_number = request.form.get('phone_number')
+        email = request.form.get('email')
+        website = request.form.get('website')
+        print(request.form.getlist('tags'))
+
+
+        new_school = School(name=name, address=address, city=city, state=state, zip_code=zip_code, phone_number=phone_number, email=email, website=website, user_id=current_user.id)
+        
+
+        for tag in request.form.getlist('tags'):
+            new_school.append(tag)
+
+        db.session.add(new_school)
+        db.session.commit()
+
+        flash("School Added!", category='success')
     return render_template("home.html", user=current_user)
 
 
